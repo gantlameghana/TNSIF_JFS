77c3ae7956b1d9ecee294acebe042f64c12bf08b
Meghana <Meghana@GANTLA-MEGHANA>
2024-02-28 11:25:10 +0530

    First commit

--- tnsif/src/com/tnsif/collections/Autoboxing.java
+++ tnsif/src/com/tnsif/collections/Autoboxing.java
@@ -3,8 +3,8 @@ package com.tnsif.collections;
 public class Autoboxing {
 	public static void main(String[] args) {
 		int a=20;
-		Integer i=Integer.valueOf(a);//converting int into Integer
-		Integer j=a;
+		Integer i=Integer.valueOf(a);//converting int into Integer explicitly
+		Integer j=a; //another method for autoboxing
 		System.out.println(a+ " "+i+ " "+j);
 	}
 
